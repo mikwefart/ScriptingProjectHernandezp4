8f08ee61439fdb56563e9823d4cf0c595a95db37
mikwefart <116204698+mikwefart@users.noreply.github.com>
2022-11-02 11:20:05 -0700

    finished all the Loop scripts
    Finished writing all the Loop Scripts.

--- ScriptingProjectHernandez/Assets/Scripts/DoWhileLoop.cs
+++ ScriptingProjectHernandez/Assets/Scripts/DoWhileLoop.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ForLoop : MonoBehaviour
+public class DoWhileLoop : MonoBehaviour
 {
-    int numEnemies = 3;
-
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < numEnemies; i++)
+        bool shouldContinue = false;
+
+        do
         {
-            Debug.Log("Creating enemy number: " + i);
-        }
+            print("Hello world");
+
+        } while (shouldContinue == true);
     }
 }
--- ScriptingProjectHernandez/Assets/Scripts/WhileLoop.cs
+++ ScriptingProjectHernandez/Assets/Scripts/WhileLoop.cs
@@ -2,16 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ForLoop : MonoBehaviour
+public class WhileLoop : MonoBehaviour
 {
-    int numEnemies = 3;
-
+    int cupsInTheSink = 4;
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < numEnemies; i++)
+        while(cupsInTheSink > 0)
         {
-            Debug.Log("Creating enemy number: " + i);
+            Debug.Log("I've washed a cup!");
+            cupsInTheSink--;
         }
     }
 }
