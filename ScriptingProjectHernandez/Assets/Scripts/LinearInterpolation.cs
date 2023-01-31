da86c48d687802936253e8b9bc30265f2ff81bfe
mikwefart <116204698+mikwefart@users.noreply.github.com>
2023-01-31 11:45:56 -0800

    linearinterpolation

---
+++ ScriptingProjectHernandez/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{/*
+    // In this case, result = 4
+    float result = Mathf.Lerp(3f, 5f, 0.5f);
+
+    Vector3 from = new Vector3 (1f, 2f, 3f);
+    Vector3  to = new Vector3 (5f, 6f, 7f);
+
+    //(4, 5, 6) because 4 is 75% of the way between 1 and 5;5 if 75% of the way between 2 and 6 and 6 is 75% of the way between 3 and 7
+
+    //Here result = (4, 5, 6)
+    Vector3 result = Vector3.Lerp (from, to, 75%)
+
+    Color.Lerp and Vector.Lerp these work in the same way as Mathf.Lerp
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Mathf.Lerp takes 3 float parameters each representing there interpolate and gives them their value
+
+    // Update is called once per frame
+    void Update()
+    {
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 0.5f);
+        light.intensity = Mathf.Lerp(light.intensity, 8f, 00.5f * Time.deltaTime);
+    }
+    //This would mean the change to intensity would happen per second instead of per frame
+*/
+}
